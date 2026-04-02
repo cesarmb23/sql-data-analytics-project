7490f838bc08be9bda2f6f3a1eeebbd2bd686f0e
Cesar M <datawithcesar@gmail.com>
2026-04-01 19:36:56 -0500

    Update 06_ranking_analysis.sql

--- scripts/06_ranking_analysis.sql
+++ scripts/06_ranking_analysis.sql
@@ -3,8 +3,13 @@
 Ranking Analysis
 ===============================================================================
 Purpose:
+	- Order the values of dimensions by measure
     - To rank items (e.g., products, customers) based on performance or other metrics.
-    - To identify top performers or laggards.
+    - Top N performers | Bottom N Performers
+
+Formula:
+	- Rank[Dimension] By Aggregate[Measure]
+	- Ex: Rank Countries by Total Sales | Top 5 products by Quantity
 
 SQL Functions Used:
     - Window Ranking Functions: RANK(), DENSE_RANK(), ROW_NUMBER(), TOP
